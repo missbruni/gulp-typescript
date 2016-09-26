5ac35c9cf2127c74d74170b7b4d6fdaf7519e3a9
Bruna Lima <bruna.lima@veinteractive.com>
2016-09-26 23:18:26 +0100

    testing uglify

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,35 @@
 const gulp = require('gulp');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
-const watchify = require('watchify');
 const tsify = require('tsify');
-const gutil = require('gulp-util');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const buffer = require('vinyl-buffer');
 
 const paths = {
   pages: ['src/*.html']
 };
-
-const watchedBrowserify = watchify(browserify({
-    basedir: '.',
-    debug: true,
-    entries: ['src/main.ts'],
-    cache: {},
-    packageCache: {}
-}).plugin(tsify));
 
 gulp.task('copy-html', () =>
   gulp.src(paths.pages)
     .pipe(gulp.dest('dist'))
 );
 
-const bundle = () =>
-  watchedBrowserify
+gulp.task('default', ['copy-html'], () =>
+  browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+  })
+    .plugin(tsify)
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('dist'));
-
-gulp.task('default', ['copy-html'], bundle);
-watchedBrowserify.on('update', bundle);
-watchedBrowserify.on('log', gutil.log);
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaos: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'))
+);
 
